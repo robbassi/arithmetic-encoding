a7e61f09ff03e4a7eee7412e861227a1a7aea056
Rob Bassi <rob.bassi.89@gmail.com>
2020-02-14 15:54:51 -0500

    Handle cli args

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,24 @@ import Encoder
 import Decoder
 import qualified Data.Map as M
 
+import System.Environment
+
+usage prog = 
+  mconcat ["USAGE: " ++ prog ++ " -[s|a|d] SRC DST\n"
+          ,"\t-s encode with a static model\n"
+          ,"\t-a encode with an adaptive model\n"
+          ,"\t-d decode\n"]
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  prog <- getProgName
+  args <- getArgs
+  case args of 
+    ["-s", src, dst] -> do
+      staticModel <- S.fromFile src
+      encodeFile staticModel src dst
+    ["-a", src, dst] -> do
+      encodeFile A.adaptiveModel src dst
+    ["-d", src, dst] -> do
+      decodeFile src dst
+    _ -> putStrLn (usage prog)
